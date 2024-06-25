7b6fc64e8caa50984695c54a806253c6a63840ea
Sona39 <skazarian99@mail.ru>
2024-06-25 18:04:15 +0900

    added alembic head command

--- app/models.py
+++ app/models.py
@@ -34,3 +34,9 @@ class Vote(Base):
     __tablename__ = "votes"
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
     post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
+
+
+class Comments(Base):
+    __tablename__ = "comments"
+
+    id = Column(Integer, primary_key=True, nullable=False)
